41c4ecd37b20bcde5d6c6b25798a4eb5d02a5380
Luis Ricardo <luisricardofer23@gmail.com>
2023-09-16 16:07:31 -0300

    just a few corrections
    just some code corrections and additions to new functions.

---
+++ Word_Search/README.txt
@@ -0,0 +1,5 @@
+This is just a simple word search game code. 
+Just type 5 words and it will generate a word grid containing the five words you entered.
+
+I used the Random library to make a function that generates a random grid of letters.
+I used the os and platform libraries to check the platform and clean up the terminal.
--- Word_Search/word_search.py
+++ Word_Search/word_search.py
@@ -1,25 +1,30 @@
-import random
+import random, platform, os
 
 # Function to create a grid of letters with hidden words.
 def creat_word_search(words, size):
     grid = [[' 'for _ in range(size)] for _ in range(size)] 
 
     for word in words:
-        direction = random.choice(['horizontal', 'vertical'])
-        if direction == 'horizontal':
-            row = random.randint(0, size -1)
-            col = random.randint(0, size - len(word))
-        else:
-            row = random.randint(0, size - len(word))
-            col = random.randint(0, size -1)
-
-
-        for letter in word:
-            grid[row][col] = letter
+        for _ in range(10):
+            direction = random.choice(['horizontal', 'vertical'])
             if direction == 'horizontal':
-                col += 1
+                row = random.randint(0, size -1)
+                col = random.randint(0, size - len(word))
             else:
-                row += 1
+                row = random.randint(0, size - len(word))
+                col = random.randint(0, size -1)
+
+            if all(grid[row + (i if direction == 'vertical' else 0)][col + (i if direction == 'horizontal' else 0)] == ' ' for i in range(len(word))):
+                for i, letter in enumerate(word):
+                    grid[row + (i if direction == 'vertical' else 0)][col + (i if direction == 'horizontal' else 0)] = letter
+                break
+
+            for letter in word:
+                grid[row][col] = letter
+                if direction == 'horizontal':
+                    col += 1
+                else:
+                    row += 1
     
     for i in range(size):
         for j in range(size):
@@ -34,8 +39,29 @@ def display_w_search(grid):
         print(' '.join(l))
     print()
 
+# str input in ordinal order
+list_words = []
+for i in range(5):
+    ordinal = 'st'
+    if  i == 1:
+        ordinal = 'nd'
+    if  i == 2:
+        ordinal = 'rd'
+    if  i == 3:
+        ordinal = 'th'
+    if  i == 4:
+        ordinal = 'th'
 
-list_words = ['BALL', 'HOUSE', 'BEE', 'TRAIN', 'DANCE']
+    word = input(f'Enter the {i + 1}{ordinal} word: ').upper()
+    list_words.append(word)
+
+
+# Check the system to clean the terminal.
+if platform.system() == 'Windows':
+    os.system('cls')
+else:
+    os.system('clear')
+
 size_grid = 15
 
 grid_wordsearch = creat_word_search(list_words, size_grid)
